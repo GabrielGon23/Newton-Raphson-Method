c2ee50b65cc4f4a71c2b4eea1ba1c7317ec9f937
Queue :) <104765325+Queue09@users.noreply.github.com>
2023-01-24 18:19:35 -0300

    Update Newton_Raphson.py

--- Newton_Raphson.py
+++ Newton_Raphson.py
@@ -1,41 +1,41 @@
 import matplotlib.pyplot as plt
 from math import e
 
-#Funcao exemplo
+# Example function
 def f(x):
-    return(((x**2 + x -1)**11)/(1 + e**-x))
+     return(((x**2 + x -1)**11)/(1 + e**-x))
 
-#Derivada da funcao
+# Derivative of the function
 def df(x):
-    return(((22*x*e**(2*x) + 22*x*e**x + 11*e**(2*x) + 11*e**x) * ((x**2+x-1)**10) + ((x**2 + x -1)**11) * (e**x)) / ((e**x + 1)**2))
+     return(((22*x*e**(2*x) + 22*x*e**x + 11*e**(2*x) + 11*e**x) * ((x** 2+x-1)**10) + ((x**2 + x -1)**11) * (e**x)) / ((e**x + 1)**2))
 
-#Aproximacao inicial
+# Initial approach
 x0 = -0.5
 
-#Precisao do erro
-precisao = 1e-6
+# Accurate error
+precision = 1e-6
 
 k = 1
-#Armazena erros e iteracoes
+# Store errors and iterations
 errors = []
 k_graph = []
 
-#Aproximacao de newton
+# Newton approximation
 x0_newton = x0
 x0_newton = x0_newton - (f(x0_newton) / df(x0_newton))
-print("\n\t\t\t >>>   Metodo de Newton - Tabela   <<<") 
-print("\n\tIteracao \t    x0_newton \t\t\t     f(x[k]) \t\tErro absoluto aproximado\n")
-while((abs((f(x0_newton) / df(x0_newton))) > precisao)):
-    x0_newton = x0_newton - (f(x0_newton) / df(x0_newton))
-    errors.append((f(x0_newton) / df(x0_newton)))
-    k_graph.append(k) 
-    print('\t   ', k, '  \t', x0_newton, '   \t', f(x0_newton), '   ', (f(x0_newton) / df(x0_newton)))
-    k = k+1
-print("\n\tAproximacao final da Raiz:",round(x0_newton,6))
+print("\n\t\t\t >>> Newton's Method - Table <<<")
+print("\n\tIteration \t x0_newton \t\t\t f(x[k]) \t\tApproximate absolute error\n")
+while((abs((f(x0_newton) / df(x0_newton))) > precision)):
+     x0_newton = x0_newton - (f(x0_newton) / df(x0_newton))
+     errors.append((f(x0_newton) / df(x0_newton)))
+     k_graph.append(k)
+     print('\t ', k, ' \t', x0_newton, ' \t', f(x0_newton), ' ', (f(x0_newton) / df(x0_newton)))
+     k = k+1
+print("\n\tFinal Root Approximation:",round(x0_newton,6))
     
-#Plota grafico de erro absoluto por iteracoes
+# Plot absolute error graph by iterations
 plt.plot(k_graph , errors)
-plt.title("Método de Newton - Gráfico do Erro absoluto aproximado")
-plt.xlabel("Iterações")
-plt.ylabel(["Erro aproximado"])
+plt.title("Newton's Method - Approximate Absolute Error Graph")
+plt.xlabel("Iterations")
+plt.ylabel(["Approximate error"])
 plt.show()
